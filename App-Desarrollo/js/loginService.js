b9babd3932022d9f49e7a6f32e20d94e2a254f8d
lauraj501 <velasquezcoronell@gmail.com>
2025-04-09 20:37:02 -0500

    Add files via upload

--- App-Desarrollo/js/loginService.js
+++ App-Desarrollo/js/loginService.js
@@ -2,8 +2,7 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    let message = ''
-    let alertType = ''
+    
 
     if (email === "" || password === ""){
         alertType = 'warning'
@@ -26,3 +25,34 @@ document.getElementById("formLogin").addEventListener('submit', function(e){
     
     document.getElementById('alert').innerHTML=alert;
 })
+
+function login (email, password){
+    let message = ''
+    let alertType = ''
+    fetch("htpps: // reqres.in/api/login", {
+        method: "POST",
+        headers: {
+            "content-type":"aplication/json"
+        },
+        body: JSON.stringify({email, password})
+    })
+    .then((data) => {
+        alertType = 'success'
+        message='inicio de sesion exitoso';
+        console.log('responde bien'+data)
+    })
+    .catch((error) =>{
+        alertType = 'danger'
+        message = 'correo o contraseña incorrectos.';
+        console.error(error)
+    })
+    let alert = `
+        <div class="alert alert-${alertType} alert-dismissible fade show" role ="alert">
+            ${message}
+            <button type= "button" class="btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+        </div>
+    `;
+    
+    document.getElementById('alert').innerHTML=alert;
+}
+   
